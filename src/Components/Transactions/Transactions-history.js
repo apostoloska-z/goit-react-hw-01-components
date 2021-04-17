152fab644c107158cd9af1fbb94662179a60d8fe
Zoia Apostoloska <zoiaapostoloska@gmail.com>
2021-04-18 01:26:17 +0300

    Updates tr-history

--- src/Components/Transactions/Transactions-history.js
+++ src/Components/Transactions/Transactions-history.js
@@ -21,8 +21,18 @@ const TransactionHistory = ({items}) => (
         )}
     </tbody>
 </table>
-    
 )
+
+TransactionHistory.propTypes = {
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.string.isRequired,
+          type: PropTypes.string.isRequired,
+          amount: PropTypes.string.isRequired,
+          currency: PropTypes.string.isRequired,
+        })
+      ).isRequired,
+}
 
 
 export default TransactionHistory;
